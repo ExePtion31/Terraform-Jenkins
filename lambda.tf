cbe831cefc99c617025c0465a185a7c459e50f0b
Giovanni Baquero <giovancito113@gmail.com>
2021-12-28 19:19:35 -0500

    :arrow_up: Increase version runtime

--- lambda.tf
+++ lambda.tf
@@ -2,7 +2,7 @@ resource "aws_lambda_function" "lambda_trigger" {
   function_name    = "${var.stack_id}-signed"
   role             = aws_iam_role.lambda_trigger_role.arn
   handler          = "index.handler"
-  runtime          = "nodejs14.x"
+  runtime          = "nodejs12.x"
   source_code_hash = filebase64sha256(var.PATH_TO_LAMBDA["zip"])
   filename         = var.PATH_TO_LAMBDA["zip"]
 
