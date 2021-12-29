6671280e6bb911aa7e8cb4ffdd752bf1caf9689c
Giovanni Baquero <giovancito113@gmail.com>
2021-12-28 19:44:55 -0500

    :wrench: Modify Bucket notification

--- lambda_trigger.tf
+++ lambda_trigger.tf
@@ -13,6 +13,6 @@ resource "aws_s3_bucket_notification" "bucket_data_notification" {
   lambda_function {
     lambda_function_arn = aws_lambda_function.lambda_trigger.arn
     events              = ["s3:ObjectCreated:*"]
-    filter_suffix       = ".json"
+    filter_suffix       = ".txt"
   }
 }
