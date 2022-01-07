44a84c3a7d19ce715a2f3cf3670c60287749af2b
Giovanni Baquero <giovancito113@gmail.com>
2022-01-06 19:43:29 -0500

    :sparkles: Adding Dynamo table

---
+++ dynamo.tf
@@ -0,0 +1,29 @@
+resource "aws_dynamodb_table" "products_table" {
+  name         = "${var.stack_id}-products-table"
+  hash_key     = "DATA_TYPE"
+  billing_mode = "PAY_PER_REQUEST"
+  tags         = local.common_tags
+
+  attribute {
+    name = "ID_PRODUCT"
+    type = "S"
+  }
+
+  attribute {
+    name = "NAME_PRODUCT"
+    type = "S"
+  }
+
+  attribute {
+    name = "PRICE_PRODUCT"
+    type = "S"
+  }
+
+  attribute {
+    name = "AMOUNT_PRODUCT"
+    type = "N"
+  }
+
+  stream_enabled   = true
+  stream_view_type = "NEW_IMAGE"
+}
