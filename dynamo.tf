93b82937ffd7126343a2776d6aaaa45928d0aa5f
Giovanni Baquero <giovancito113@gmail.com>
2022-01-10 22:43:48 -0500

    :wrench: Config Dynamo table

--- dynamo.tf
+++ dynamo.tf
@@ -1,5 +1,5 @@
 resource "aws_dynamodb_table" "products_table" {
-  name           = "${var.stack_id}-products-tables"
+  name           = "${var.stack_id}-products-table"
   hash_key       = "ID_PRODUCT"
   range_key      = "NAME_PRODUCT"
   read_capacity  = 30
